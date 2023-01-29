6478c6b50d9b36e6c133a4b9e5231e1f60cde69e
Loop_Jackck <c26269743@gmail.com>
2023-01-29 17:33:20 +0900

    2023-01-29 -5

---
+++ ksa/CShap_Study0129 - 2/CShap_Study0129 - 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShap_Study0129___2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //식당에 음식이 떡볶이, 김밥, 라면, 라뽁이가 있다
+            //이중에 숫자를 입력받으면
+            //입력받은 값은 비어 있게 된다.
+
+            //최초 음식들과
+            //숫자를 입력받았을때 음식을 출력하고
+
+            //비어있게 되어있다라는 결과 값도 출력하라
+        }
+    }
+}
