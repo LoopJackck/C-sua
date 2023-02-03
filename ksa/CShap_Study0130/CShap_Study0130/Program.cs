b4bdf6e8c48c0c3def7631f150f6e3004fc4a184
LoopJackck <123847833+LoopJackck@users.noreply.github.com>
2023-02-03 11:36:04 +0900

    2023-02-03
    2023-02-03

---
+++ ksa/CShap_Study0130/CShap_Study0130/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShap_Study0130
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("제 이름은 김수아 입니다");
+            System.Console.Write("저는 이제 고3이 되었습니다 ");
+            System.Console.WriteLine("저는 효성고등학교에 다니고 있습니다");
+            System.Console.WriteLine("저는 게임 기획자가 되기위해 포폴 준비를 하고 있습니다");
+            System.Console.WriteLine("포폴 준비를 위해 학원을 다니고 있습니다");
+            System.Console.WriteLine("저는 강아지를 키웁니다");
+            System.Console.WriteLine("강아지의 이름은 리키 입니다");
+            System.Console.WriteLine("저는 엄마, 아빠, 동생과 삽니다");
+            System.Console.WriteLine("저는 그림그리는 걸 좋아합니다");
+            System.Console.WriteLine("저는 코딩하는 것에도 흥미가 있습니다");
+
+        }
+    }
+}
---
+++ ksa/CShap_Study0201/CShap_Study0201/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShap_Study0201
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String myName;
+            string birthYear;
+            String myjen;
+            String myzip;
+            String my;
+
+            System.Console.WriteLine("이름을 입력해주세요 : ");
+            myName = System.Console.ReadLine();
+
+            System.Console.WriteLine("태어난 연도를 입력해주세요");
+            birthYear = System.Console.ReadLine();
+
+            System.Console.WriteLine("주소를 입력해주세요 : ");
+            myzip = System.Console.ReadLine();
+
+            System.Console.WriteLine("성별을 입력해주세요 : ");
+            myjen = System.Console.ReadLine();
+
+            System.Console.WriteLine("자기 소개를 입력해 주세요 : ");
+            my = System.Console.ReadLine();
+
+            int myAgeNumber = Convert.ToInt32(birthYear);
+            myAgeNumber = 2023 - myAgeNumber + 1;
+
+
+            System.Console.Write("당신의 이름은 ");
+            System.Console.Write(myName);
+            System.Console.WriteLine("입니다");
+
+            System.Console.Write("당신의 나이는 ");
+            System.Console.Write(myAgeNumber);
+            System.Console.WriteLine("입니다");
+
+            System.Console.Write("당신의 주소는 ");
+            System.Console.Write(myzip);
+            System.Console.WriteLine("입니다");
+
+            System.Console.Write("당신의 성별은 ");
+            System.Console.Write(myjen);
+            System.Console.WriteLine("입니다");
+
+            System.Console.WriteLine(my);
+        }
+    }
+}
