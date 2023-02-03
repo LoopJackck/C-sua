0a2d1d703401766caf76fa7fa05872a847c2af72
LoopJackck <123847833+LoopJackck@users.noreply.github.com>
2023-02-03 23:33:26 +0900

    2023-02-03
    2023-02-03

---
+++ ksa/CShap_Study0203/CShap_Study0203/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CShap_Study0203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A;
+            string[] food;
+            food = new string[4] { "떡볶이", "김밥", "만두", "튀김" };
+            System.Console.WriteLine(food[0]);
+            System.Console.WriteLine(food[1]);
+            System.Console.WriteLine(food[2]);
+            System.Console.WriteLine(food[3]);
+            System.Console.WriteLine("먹을 음식은?");
+            A = System.Console.ReadLine();
+            int Aa = Convert.ToInt32(A);
+
+            System.Console.Write(food[Aa]);
+            System.Console.WriteLine("을(를) 먹었다");
+
+            food[Aa] = " ";
+
+            System.Console.WriteLine(food[0]);
+            System.Console.WriteLine(food[1]);
+            System.Console.WriteLine(food[2]);
+            System.Console.WriteLine(food[3]);
+        }
+    }
+}
